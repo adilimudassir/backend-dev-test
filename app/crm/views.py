ec5773b6f88cb33c682f19933eec4c269e5b1c04
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-07-05 11:09:58 +0100

    add views

--- app/crm/views.py
+++ app/crm/views.py
@@ -1,3 +1,36 @@
+
 from django.shortcuts import render
 
+from app.crm.models import Client
+
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+
 # Create your views here.
+class ClientListView(ListView):
+    model = Client
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = "__all__"
+    
+    success_url = reverse_lazy('crm:client_list')
+
+class ClientDetailView(DetailView):
+    model = Client
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = "__all__"
+    
+    success_url = reverse_lazy('crm:client_list')
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    fields = "__all__"
+    
+    success_url = reverse_lazy('crm:client_list')
