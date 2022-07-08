6f173f47ccc5920a442ad65058ea7011b3c00eaa
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-07-08 01:11:01 +0100

    add error messages to crud

--- app/crm/forms.py
+++ app/crm/forms.py
@@ -3,30 +3,97 @@ from django import forms
 from .models import Client, ClientWallet
 
 
-class AddClientForm(forms.ModelForm):
+class ClientForm(forms.ModelForm):
+    cid = forms.CharField(
+        widget = forms.TextInput(
+            attrs = {
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
+                "placeholder": "Client ID",
+            }
+        ),
+        label = "Client ID",
+        required = True,
+    )
+    first_name = forms.CharField(
+        widget = forms.TextInput(
+            attrs = {
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
+                "placeholder": "First Name",
+            }
+        )
+    )
+    last_name = forms.CharField(
+        widget = forms.TextInput(
+            attrs = {
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
+                "placeholder": "Last Name",
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget = forms.EmailInput(
+            attrs = {
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
+                "placeholder": "Email",
+            }
+        )
+    )
+    phone = forms.CharField(
+        widget = forms.TextInput(
+            attrs = {
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
+                "placeholder": "Phone",
+            }
+        )
+    )
+    address = forms.CharField(
+        widget = forms.Textarea(
+            attrs = {
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
+                "placeholder": "Address",
+            }
+        )
+    )
+    country_code = forms.CharField(
+        widget = forms.TextInput(
+            attrs = {
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
+                "placeholder": "Country Code",
+            }
+        )
+    )
+
+
 
     class Meta:
         model = Client
         fields = ('cid', 'first_name', 'last_name', 'country_code', 'email', 'address', 'phone')
 
-        # widgets = {
-        #     'cid': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}),
-        #     'first_name': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}),
-        #     'last_name': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}),
-        #     'country_code': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}),
-        #     'email': forms.EmailInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}),
-        #     'address': forms.Textarea(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}),
-        #     'phone': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}),
-        # }
-
 class ClientWalletForm(forms.ModelForm):
-
+    total_balance = forms.DecimalField(
+        widget = forms.NumberInput(
+            attrs = {
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
+                "placeholder": "Total Balance",
+            }
+        )
+    )
+    available_balance = forms.DecimalField(
+        widget = forms.NumberInput(
+            attrs = {
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
+                "placeholder": "Available Balance",
+            }
+        )
+    )
+    lien_balance = forms.DecimalField(
+        widget = forms.NumberInput(
+            attrs = {
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
+                "placeholder": "Lien Balance",
+            }
+        )
+    )
     class Meta:
         model = ClientWallet
         fields = ('total_balance', 'available_balance', 'lien_balance')
-
-        widgets = {
-            'total_balance': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}),
-            'available_balance': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}),
-            'lien_balance': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}),
-        }
--- app/crm/views.py
+++ app/crm/views.py
@@ -1,132 +1,98 @@
 
 from django.shortcuts import render
+from django.contrib import messages
 
 from app.crm.models import Client, ClientWallet
-from app.crm.forms import AddClientForm, ClientWalletForm
+from app.crm.forms import ClientForm, ClientWalletForm
 from django.shortcuts import redirect
-from django.urls import reverse_lazy
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
-from django.views.generic.list import ListView
-from django.views.generic.edit import UpdateView
-from django.views.generic.edit import FormView
-from django.views.generic.edit import DeleteView
 
-# Create your views here.
-
-# def home():
-#     return redirect("clients:client_list")
-
-# class ClientListView(ListView):
-#     model = Client
-#     template_name = "clients/list.html"
-
-# class ClientCreateView(CreateView):
-#     # model = Client
-#     # fields = "__all__"
-#     form_class = AddClientForm
-#     template_name = "clients/form.html"
-#     success_url = reverse_lazy("clients:client_list")
-
-# class ClientDetailView(DetailView):
-#     model = Client
-#     template_name = "clients/detail.html"
-
-# class ClientUpdateView(UpdateView):
-#     model = Client
-#     fields = "__all__"
-#     template_name = "clients/form.html"
-#     success_url = reverse_lazy("clients:client_list")
-
-# class ClientDeleteView(DeleteView):
-#     model = Client
-#     fields = "__all__"
-#     template_name = "clients/confirm_delete.html"
-    
-#     success_url = reverse_lazy("clients:client_list")
 
 def client_index(request):
     clients = Client.objects.all()
 
-    context = {
+    return render(request, "clients/index.html", {
         "clients": clients,
-    }
-    return render(request, "clients/index.html", context)
+    })
 
 def client_detail(request, pk):
     client = Client.objects.get(pk=pk)
 
-    context = {
+    return render(request, "clients/detail.html", {
         "client": client,
-    }
-    return render(request, "clients/detail.html", context)
+    })
 
 def client_create(request):
     if request.method == "POST":
-        form = AddClientForm(request.POST)
+        form = ClientForm(request.POST)
+        
         if form.is_valid():
             form.save()
+
             #add client to wallet
-            wallet = ClientWallet.objects.create(client=form.instance)
+            ClientWallet.objects.create(client=form.instance)
+            
+            messages.success(request, "Client created successfully")
             return redirect("clients:client_list")
-        # client = Client.objects.create(
-        #     cid = request.POST["cid"],
-        #     first_name = request.POST["first_name"],
-        #     last_name = request.POST["last_name"],
-        #     email = request.POST["email"],
-        #     phone = request.POST["phone"],
-        #     country_code = request.POST["country_code"],
-        #     address = request.POST["address"],
-        # )
-        # return redirect("clients:client_detail", pk=client.pk)
+        else:
+            messages.error(request, "Error creating client")
+    
     else:
-        form = AddClientForm()
-    context = {
-        "form": form,
-    }
-    return render(request, "clients/form.html", context)
+        form = ClientForm()
+    
+    return render(request, "clients/form.html", {
+        "form": form
+    })
 
 def client_update(request, pk):
     client = Client.objects.get(pk=pk)
+    
     if request.method == "POST":
-        form = AddClientForm(request.POST, instance=client)
+        form = ClientForm(request.POST, instance=client)
+        
         if form.is_valid():
             form.save()
-            return redirect("clients:client_list")
+            messages.success(request, "Client updated successfully")
+            return redirect("clients:client_detail", pk=client.pk)
+        else:
+            messages.error(request, "Error updating client")
+
     else:
-        form = AddClientForm(instance=client)
-    context = {
+        form = ClientForm(instance=client)
+    
+    return render(request, "clients/form.html", {
         "form": form,
         "client": client,
-    }
-    return render(request, "clients/form.html", context)
+    })
 
 def client_delete(request, pk):
     client = Client.objects.get(pk=pk)
-    # if request.method == "POST":
     #delete client wallet first
     client_wallet = ClientWallet.objects.get(client=client)
     client_wallet.delete()
 
     #delete client
     client.delete()
+
+    messages.success(request, "Client deleted successfully")
     return redirect("clients:client_list")
-    # context = {
-    #     "client": client,
-    # }
-    # return render(request, "clients/confirm_delete.html", context)
 
 
 def client_wallet_update(request, pk):
     client_wallet = ClientWallet.objects.get(pk=pk)
+    
     if request.method == "POST":
         form = ClientWalletForm(request.POST, instance=client_wallet)
+        
         if form.is_valid():
             form.save()
+            messages.success(request, "Client wallet updated successfully")
             return redirect("clients:client_detail", pk=client_wallet.client.pk)
+        else:
+            messages.error(request, "Error updating client wallet")
+    
     else:
         form = ClientWalletForm(instance=client_wallet)
-    context = {
-        "form": form,
-    }
-    return render(request, "clients/wallet_form.html", context)
+
+    return render(request, "clients/wallet_form.html", {
+        "form": form
+    })
