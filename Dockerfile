f9dca1400e6b3ad9b1f0718b4e0765caaabb69e5
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-07-10 00:57:26 +0100

    fix websocket not working on proxy server

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,8 @@ COPY ./requirements.txt /code/
 RUN pip install -U pip
 RUN python3 -m pip install --no-cache-dir -r requirements.txt
 
+RUN pip install -U 'Twisted[tls,http2]'
+RUN pip install 'uvicorn[standard]'
 COPY . /code
 
 COPY ./entrypoint.sh /
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@
 python manage.py migrate  --no-input
 python manage.py collectstatic --no-input
 
-gunicorn devtest.wsgi:application --bind 0.0.0.0:8000
+gunicorn devtest.asgi:application --bind 0.0.0.0:8000 -w 2 -k uvicorn.workers.UvicornWorker
+
+# daphne -b 0.0.0.0 -p 8000 devtest.asgi:application
